c3c4627118ed9e050ed9c315b1150abd0765ac1c
schneiderk26130 <schneiderk26130@student.cghsnc.org>
2022-09-28 14:10:40 -0400

    Autonomous for 4 stations, red and blue upper and lower

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerPlay11588/Autonomous/RI3W11588BlueUpper.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.PowerPlay11588.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name = "RI3W 11588 Blue Upper", group = "11588")
+public class RI3W11588BlueUpper extends RI3W11588BaseAutonomous{
+    ElapsedTime runTime = new ElapsedTime();
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot.init(this.hardwareMap);
+        waitForStart();
+        runTime.reset();
+        while(runTime.milliseconds() < 2000){}
+        /*
+        if(spot1){
+            move to spot 1
+        }else if(spot2) {
+            move to spot 2
+        }else if(spot3){
+            move to spot 3
+        }
+         */
+        //if spot 1
+        moveXY(0, -24);
+        moveXY(36, 0);
+
+        //if spot 2
+        moveXY(36, 0);
+
+        //if spot 3
+        moveXY(0, 24);
+        moveXY(36, 0);
+    }
+}
