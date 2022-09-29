093d3e179a2c38e737e190c5f0d510739c24e456
schneiderk26130 <schneiderk26130@student.cghsnc.org>
2022-09-29 15:11:07 -0400

    Autonomous for 4 stations, red and blue upper and lower

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerPlay11588/Autonomous/RI3W11588RedUpper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerPlay11588/Autonomous/RI3W11588RedUpper.java
@@ -4,6 +4,8 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name = "RI3W 11588 Red Upper", group = "11588")
 public class RI3W11588RedUpper extends RI3W11588BaseAutonomous{
+
+
     ElapsedTime runTime = new ElapsedTime();
     @Override
     public void runOpMode() throws InterruptedException {
