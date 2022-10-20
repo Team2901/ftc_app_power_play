e8e1610066c26731c2040b0a48368cc2e71ae75e
setiai23785 <setiai23785@student.cghsnc.org>
2022-10-20 15:18:06 -0400

    sort of working pid loop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerPlay2901/Autonomous/IntelRealsenseAuto.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.PowerPlay2901.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.PowerPlay2901.EarlyDiffy.EarlyDiffyHardware;
+
+public class IntelRealsenseAuto extends OpMode {
+    EarlyDiffyHardware robot = new EarlyDiffyHardware();
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
