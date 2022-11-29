628c1e66a17db703c00ae4450e39c498af25c344
Niknut <allenn23825@student.cghsnc.org>
2022-11-29 15:22:42 -0500

    Merrrrrrrrrrrrggggggggeeeeeeee

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerPlay11588/Autonomous/Qual11588BluePark.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.PowerPlay11588.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name = "RI3W 11588 Blue ", group = "11588")
+public class Qual11588BluePark extends Qual11588BaseAuto{
+    ElapsedTime runTime = new ElapsedTime();
+    @Override
+    public void runOpMode() throws InterruptedException {
+        this.teamColor = AllianceColor.BLUE;
+        robot.init(this.hardwareMap, telemetry);
+        waitForStart();
+        runTime.reset();
+        moveArm(Height.GROUND);
+        while(runTime.milliseconds() < 2000){
+
+        }
+
+        park();
+    }
+}
