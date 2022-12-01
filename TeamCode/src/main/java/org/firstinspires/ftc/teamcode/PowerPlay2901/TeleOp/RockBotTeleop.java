f365576a50752edf2c3ccb2afcb516b3b05f6f62
meyersw23562 <55461160+meyersw23562@users.noreply.github.com>
2022-12-01 14:57:18 -0500

    added the name to the rock bot teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerPlay2901/TeleOp/RockBotTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerPlay2901/TeleOp/RockBotTeleop.java
@@ -1,11 +1,13 @@
 package org.firstinspires.ftc.teamcode.PowerPlay2901.TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.PowerPlay2901.Hardware.RockBotHardware;
 
+@TeleOp(name = "Dwayne TeleOp", group = "RockBot")
 public class RockBotTeleop extends OpMode {
     RockBotHardware robot = new RockBotHardware();
     private ElapsedTime runtime = new ElapsedTime();
