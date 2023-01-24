901c9b3007dfa12581445fff7de16015581c8988
NickJerden <jerdenn25556@student.cghsnc.org>
2023-01-24 16:40:02 -0500

    clawbot stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Outreach/TeleOp/NewClawbotTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Outreach/TeleOp/NewClawbotTeleOp.java
@@ -1,4 +1,36 @@
 package org.firstinspires.ftc.teamcode.Outreach.TeleOp;
 
-public class NewClawbotTeleOp {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Outreach.Hardware.NewClawbotHardware;
+import org.firstinspires.ftc.teamcode.Shared.Gamepad.ImprovedGamepad;
+
+@TeleOp(name="New Clawbot Teleop", group="Outreach")
+public class NewClawbotTeleOp extends OpMode {
+    public enum clawState{
+        OPEN, CLOSED
+    }
+    public ImprovedGamepad gamepad;
+    public ImprovedGamepad masterGamepad;
+    public ElapsedTime gamepadTimer = new ElapsedTime();
+    public clawState currentClawState;
+
+    NewClawbotHardware robot = new NewClawbotHardware();
+    @Override
+    public void init() {
+        gamepad = new ImprovedGamepad(gamepad1, gamepadTimer, "gamepad");
+        masterGamepad = new ImprovedGamepad(gamepad2, gamepadTimer, "masterGamepad");
+        robot.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        gamepad.update();
+        masterGamepad.update();
+        robot.leftDrive.setPower(gamepad.left_stick_y.getValue());
+        robot.rightDrive.setPower(gamepad.right_stick_y.getValue());
+
+    }
 }
